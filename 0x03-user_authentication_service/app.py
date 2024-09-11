a0d33215f54adf544d485e32fb46da0f9ee82a1c
Susan <susansewe@gmail.com>
2024-09-11 20:09:39 +0000

    Tasks 9-14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ App Module"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ def index() -> str:
         - The home page's payload.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def register_user() -> str:
@@ -33,6 +34,40 @@ def register_user() -> str:
     except ValueError:
         # Handle the case where the email is already registered
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """Posts session id for a logged-in user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # Check if login information is valid
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    try:
+        # Create a new session for the user
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        # Store the session ID as a cookie in the response
+        response.set_cookie("session_id", session_id)
+        return response
+
+    except Exception as e:
+        # Handle any exceptions that may occur
+        return jsonify({"message": str(e)}), 500
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """ Ends the users session"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 from user import User
 
@@ -10,6 +11,11 @@ from user import User
 def _hash_password(password: str) -> bytes:
     """Hashes a password."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Method to generate UUID"""
+    return str(uuid4())
 
 
 class Auth:
@@ -48,3 +54,32 @@ class Auth:
             return False
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """ Generates session id"""
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        if user is None:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(sef, session_id: str) -> User:
+        """Retrives user from session id"""
+        user = None
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy/delete the user's session by setting session_id to None."""
+        # Update user's session ID to None using the public method of self._db
+        self._db.update_user(user_id, session_id=None)
